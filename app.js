046f8527b5ea5b6db8ca65dc5c2405fb963162f0
Vivian Sarazin <vivian.sarazin.dev@gmail.com>
2020-05-11 16:35:12 +0200

    s1 :: displaying randomly images

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-// Scène et contrôles
 const container = document.getElementById("container")
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 200)
+const controls = new THREE.OrbitControls(camera, container)
+const renderer = new THREE.WebGLRenderer()
+const rayCaster = new THREE.Raycaster()
 
+renderer.setSize(window.innerWidth, window.innerHeight)
+container.appendChild(renderer.domElement)
+controls.rotateSpeed = 0.2
+controls.enablePan = false
+camera.position.set(-0.1, 0, 0)
+controls.update()
 
 class Scene {
-  constructor(image, camera) {
+  constructor(image, camera, materials = []) {
     this.image = image
     this.camera = camera
     this.sprites = []
     this.points = []
+    this.materials = materials
+    this.geometry = new THREE.SphereGeometry(50, 32, 32)
   }
 
   createScene(scene) {
     this.scene = scene
-    const geometry = new THREE.SphereGeometry(50, 32, 32)
     const texture = new THREE.TextureLoader().load(this.image)
     texture.minFilter = THREE.LinearFilter;
     texture.wrapS = THREE.RepeatWrapping
@@ -23,10 +34,28 @@ class Scene {
       side: THREE.DoubleSide
     })
     material.transparent = true
-    this.sphere = new THREE.Mesh(geometry, material)
-    this.scene.add(this.sphere)
 
+    this.sphere = new THREE.Mesh(this.geometry, material)
+    this.scene.add(this.sphere)
     this.points.forEach(this.addToolTip.bind(this))
+    this.addMaterials()
+  }
+
+  randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  addMaterials() {
+    if (this.materials.length > 0) {
+      for (let i = 0; i < this.materials.length; i++) {
+        let tempGeo = new THREE.PlaneGeometry(10, 10, 20);
+        let map = new THREE.TextureLoader().load(this.materials[i])
+        let tempMaterial = new THREE.MeshBasicMaterial({ map, side: THREE.DoubleSide });
+        let plane = new THREE.Mesh(tempGeo, tempMaterial);
+        this.scene.add(plane)
+        plane.position.set(this.randomNumber(-10, 10), this.randomNumber(-10, 10), this.randomNumber(-50, 50))
+      }
+    }
   }
 
 
@@ -34,7 +63,7 @@ class Scene {
     this.points.push(point)
   }
 
-  addToolTip(point, name) {
+  addToolTip(point) {
     let spriteMap = new THREE.TextureLoader().load('info.png')
     let spriteMaterial = new THREE.SpriteMaterial({
       map: spriteMap
@@ -76,22 +105,23 @@ class Scene {
   }
 }
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 200)
-const controls = new THREE.OrbitControls(camera, container)
-const renderer = new THREE.WebGLRenderer()
-const rayCaster = new THREE.Raycaster()
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-container.appendChild(renderer.domElement)
-controls.rotateSpeed = 0.2
-controls.enablePan = false
-camera.position.set(-0.1, 0, 0)
-controls.update()
+const materials = {
+  s0: {
+
+  },
+  s1: [
+    "assets/s1/materials/mw4.jpg",
+    "assets/s1/materials/mw5.jpg",
+    "assets/s1/materials/mw6.jpg",
+    "assets/s1/materials/mw8.jpg",
+  ]
+}
+
 
 // Sphères
-let s0 = new Scene('360-S0.jpeg', camera)
-let s1 = new Scene('360-S1.jpeg', camera)
+let s0 = new Scene('assets/s0/360.jpeg', camera)
+let s1 = new Scene('assets/s1/360.jpeg', camera, materials.s1)
 
 s0.addPoint({
   position: new THREE.Vector3(2, 0.5, 0),
@@ -107,7 +137,8 @@ s1.addPoint({
 
 s0.createScene(scene)
 s0.appear()
-// onResize
+
+// onClick
 function onClick(e) {
   let mouse = new THREE.Vector2(
     (e.clientX / window.innerWidth) * 2 - 1,
